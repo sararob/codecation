acf92a6d0cd05394ca3108fac6726d890a5c3f97
sararob <sararob@brandeis.edu>
2013-03-14 11:10:47 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codecation::Application.config.secret_token = '46ce3fae0146f4a50ba393a00218c5eb09e3eca2397dcb332dc1ee1d4dce48e3efa21c15a746047d6fe4cdbbafbdffb2eba1bc6ae87d2b0d28b5f229136c0957'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Codecation::Application.config.session_store :cookie_store, key: '_codecation_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Codecation::Application.config.session_store :active_record_store
